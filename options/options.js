ad04776f4e3b90c172dd8907c155a4ad0fec642a
Crowdbotics <team@crowdbotics.com>
2023-05-07 18:56:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-pine-40524",
+  url: "https://gentle-pine-40524.botics.co",
+  api: "https://gentle-pine-40524.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
